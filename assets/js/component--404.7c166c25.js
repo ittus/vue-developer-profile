ff7540c4c6ffffb43ecf4fa946f78b51b2536c84
Vu Minh Thang <vuminhthang.cm@gmail.com>
2019-04-29 22:43:37 +0900

    Updates

---
+++ assets/js/component--404.7c166c25.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{241:function(n,t,e){"use strict";e.r(t);var s=e(67),u=Object(s.a)({},function(){var n=this.$createElement;return(this._self._c||n)("h1",[this._v("404 - not found")])},[],!1,null,null,null);t.default=u.exports}}]);
